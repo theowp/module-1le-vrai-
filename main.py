309e4bdfdacc4aa62e8cb9b9b70f5d791a7c9693
Théo <theo.alasseur@outlook.fr>
2025-10-02 13:13:50 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+# question 1
+
+# age = 22
+# height = 1.75
+# name = "theo"
+# is_student = True
+# print(age, height, name, is_student)
+
+# question 2
+
+# a = 2
+# b = 3
+# print(f"a+b={a+b}")
+# print(f"a-b={a-b}")
+# print(f"a x b={a*b}")
+# print(f"a/b={a/b}")
+# print (f"reste de a/b={a%b}")
+# print(f"a^b={a**b}")
+
+# question 3
+
+# first_name = "Alasseur" 
+# last_name = "Théo"
+# full_name = first_name+" "+last_name
+# caractère = len(full_name)
+# sentence = "hello comment ca va ?"
+# words_list = sentence.split()
+# print("full name (uppercase): ", full_name.upper())
+# print(f"caractère= {caractère}")
+# print(f"sentence split ={words_list}")
+
+# question 4
+
+# string = "9"
+# integer = int(string)
+# floats = 9.3
+# integer2 = int(floats)
+# integer3 = 10
+# floats2 = float(integer3)
+# number = 10,9
+# strings2 = str(number)
+# print(integer, integer2, floats2, strings2)
+
+# Question 5
+
+age = 18
+taille = 1.75
